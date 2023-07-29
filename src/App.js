214c5398d7226a7a57037020a62dfbb3b158ede1
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-29 18:50:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignUp from './components/auth/SignUp';
 import AuthDetails from './components/auth/AuthDetails';
 import Home from './components/Home';
 
+//en authdetails deje un toq explicado como funciona que mostras en la pagina -lo que entiendo al menos-
 function App() {
   return (
     <div className="App">
