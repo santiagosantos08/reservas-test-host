d88ae4c34b390f92053546748e78394e5d38eb02
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-29 19:10:02 -0300

    Update AuthDetails.jsx

--- src/components/auth/AuthDetails.jsx
+++ src/components/auth/AuthDetails.jsx
@@ -5,6 +5,53 @@ import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import Home from "../Home";
 
+/*
+Acá arriba va codigo de javascript de lo que quieras o necesites para que funcione la pagina
+ej: acá hay una funcion para cerrar sesion y otra que te indica si estas logueado o no
+(y que usuario sos en caso de estarlo. ese 'user' es el ID de firebase, despues cuando quieras 
+armar la parte de reservar las canchas, etc vas a tener que pasar la logica de compararlo en la sesion a algo que lo compare en la db
+
+en la pag de firebase tenes que activar las funcionalidades que quieras y vas importando lo que necesites, ej acá solo importo el modulo de auth
+despues tienen otro de realtime, alguno general, etc, dependiendo cual uses es como le guardas info, eso vas a tener que ver la documentacion del que sea que elijas (o si usas supra lo mismo)
+
+supongo que con la firestore db alcanza para esto ya que no hay mucho trafico pero en cualquier caso tienen tambien la 'realtime database' (supongo será mas enrosque y/o de pago)
+
+la firestore la usé una vez y trabaja con documentos, depende de como la quieras estructurar vas a tener las reservas asignadas al usuario o a la cancha
+
+No cambia mucho de una planilla de excel
+
+un ejemplo, dependiendo de como lo hagas puede quedar algo que nada que ver con esto
+
+await setDoc(nombreDoc(nombreDb, "canchas", "cancha1"), {
+  nombre: "user.getName()",
+  horario: horario (no se como lo sacas del calendario ese que importaste),
+  alquilaPaletas: true
+});
+
+https://firebase.google.com/docs/firestore/manage-data/add-data?hl=es-419
+
+abajo en el return es donde escribis el html de toda la vida, lo que tiene de bueno (pero puede ser confuso) React es que te deja mezclarlo bastante, podes meter htmk en js que este dentro de html y así
+
+la mayoria de los return estan escritos como
+
+return {condicion ? (op1;) : (op2;)} es un operador ternario, es lo mismo que escribirlo asi
+
+if(condicion){
+  return op1;
+} else{
+  return op2;
+}
+
+pero corre mas rapido por temas de como está implementado, en esto que van a entrar como mucho 20 personas al dia escribilo como te quede mas comodo
+
+si lo haces con 'if' siempre intenta dejar un caso base fuera de los if por cualquier cosa que pueda pasar, que no retorne un numero vacio de algun lugar de memoria que no sabes que tiene almacenado
+
+ej lo de arriba sería mejor:
+
+if(condicion) return op1;
+return op2; -> por defecto
+*/
+
 const AuthDetails = () => {
   const [authUser, setAuthUser] = useState(null);
   const [showComponent, setShowComponent] = useState(false);
