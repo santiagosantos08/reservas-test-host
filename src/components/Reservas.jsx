d14594ae0051dd29747f741928e94b486a22deba
Santiago <89073340+santiagosantos08@users.noreply.github.com>
2023-07-29 23:01:56 +0000

    default commit from codespaces

--- src/components/Reservas.jsx
+++ src/components/Reservas.jsx
@@ -1,11 +1,15 @@
 import React,{ useState, } from "react";
-import { firestore } from "../firebase";
+import { auth } from "../../firebase";
+import { db } from "../firebase";
+
+//sin terminar
 
 function Reservas() {
 
     const [reserva, setReserva] = useState("");
 
     const aggReserva = (r) => {
+        console.log("agregando reserva..");
         //agg a firestore por ahora es solo texto como p mostrar
     }
 
@@ -16,7 +20,7 @@ function Reservas() {
                     type="text"
                     placeholder="Agregar una reserva"
                     value={reserva}
-                    onChange={(r) => aggReserva(r.target.value)}
+                    onChange={(r) => setReserva(r.target.value)}
                 ></input>
                 <button type="submit">Agregar Reserva</button>
             </form>
